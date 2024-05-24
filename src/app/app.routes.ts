78b41d2b975126c4ad2149ddcc7dea9389bbcd9e
Allan Goncalves Da Silva <agsilva15@latam.stefanini.com>
2024-05-24 14:03:52 -0300

    create project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'login'},
+  {path: 'login',
+    loadChildren: () => import('./Login/login.routes').then(m => m.LOGIN_ROUTES)
+  }
+];
