161add63a8054723862149f7735c7600f8c5a06e
Allan Goncalves Da Silva <agsilva15@latam.stefanini.com>
2024-06-22 10:50:50 -0300

    Initial commit

--- src/app/Login/login/login.component.ts
+++ src/app/Login/login/login.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatInputModule, MatButtonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  formLogin = this.formBuilder.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]]
+  });
 
+  constructor(private formBuilder: FormBuilder){
+
+  }
+
+  login(){
+
+    this.formLogin.controls.username.markAsDirty();
+    this.formLogin.controls.username.markAllAsTouched();
+
+    this.formLogin.controls.password.markAsDirty();
+    this.formLogin.controls.password.markAllAsTouched();
+  }
 }
